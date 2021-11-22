efd33825691c194618d4dad7c5bfce020e18e125
pocpoc0202 <pocpoc0202@naver.com>
2021-11-22 11:24:53 +0900

    리액트 공부

---
+++ 01. basics/webpack-practices/ex02/src/App.js
@@ -0,0 +1,8 @@
+import {myComponent} from './components/myComponent'
+const App = function() {
+    const app = document.createElement('h1');
+    app.textContent = 'Hello World';
+    return app;
+}
+
+export { App }
