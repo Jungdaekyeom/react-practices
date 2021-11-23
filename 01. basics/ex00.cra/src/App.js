b572a62b03a421bfa27a8ed9dff9001f2537a24f
pocpoc0202 <pocpoc0202@naver.com>
2021-11-23 09:42:13 +0900

    commit

--- 01. basics/ex00.cra/src/App.js
+++ 01. basics/ex00.cra/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is App.js's text
+          Hello React!!!
         </a>
       </header>
     </div>
--- 01. basics/webpack-practices/ex05/src/App.js
+++ 01. basics/webpack-practices/ex05/src/App.js
@@ -1,5 +1,8 @@
+import './assets/scss/App.scss';
+
 const App = function() {
     const app = document.createElement('h1');
+    app.className = 'Header';
     app.textContent = 'Hello World';
     return app;
 }
